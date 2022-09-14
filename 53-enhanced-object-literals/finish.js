4e8a140f06e4b597f5e04409202da3ccd9964a5a
VladimirFomin90 <vladimirfomin90@yandex.ru>
2022-09-14 20:06:30 +0600

    52-57

--- 53-enhanced-object-literals/finish.js
+++ 53-enhanced-object-literals/finish.js
@@ -14,22 +14,22 @@
 
 const photosGallery = (title, dimensions, date) => {
   return {
-    title: title,
-    info: function() {
+    [dimensions]: true,
+    date,
+    title,
+    info() {
       console.log(
-        "Фото "${title}" имеет разрешение ${date}`
+        `Фото "${title}" имеет разрешение ${dimensions}`
       );
     },
-    dimensions: dimensions
-    publishInfo: () => {
+    publishInfo() {
       console.log(
-        `Фото было опубликовано ${Math.floor(
+        `Фото "${title}" было опубликовано ${Math.floor(
           (new Date().getTime() - date.getTime()) / 1000
         )} секунды назад`
       );
-    ,
-    date: date
-  }
+    },
+}
 }
 
 const myDogPhoto = photosGallery(
@@ -38,8 +38,8 @@ const myDogPhoto = photosGallery(
   new Date()
 )
 
-const testDimension1 = "1920x1080"
-const testDimension2 = "1080x720"
+const testDimension1 = "1920x1080";
+const testDimension2 = "1080x720";
 
 myDogPhoto.info()
 /* Фото "My dog" имеет разрешение 1920x1080 */
@@ -50,5 +50,5 @@ setTimeout(() => myDogPhoto.publishInfo(), 2000)
 /* ВОПРОС: Почему метод "publishInfo" все еще имеет доступ 
 к параметрам функции "photosGallery" (например "date")? */
 
-console.log(myDogPhoto[testDimension1]) // true
-console.log(myDogPhoto[testDimension2]) // undefined
+console.log(myDogPhoto[testDimension1]); // true
+console.log(myDogPhoto[testDimension2]); // undefined
